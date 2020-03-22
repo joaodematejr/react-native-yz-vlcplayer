6933bc06099f705ac9119d3fdd4fd875806a7922
joaodematejr <joaodematejr@gmail.com>
2020-03-21 22:51:32 -0300

    update fix android

--- demo/App.js
+++ demo/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, SafeAreaView } from 'react-native';
+import { VLCPlayer, VlCPlayerView } from 'react-native-yz-vlcplayer';
 
 export default function App() {
   return (
